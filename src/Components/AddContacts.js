77c3316aae54f22fe259f4b80eba593206ba0319
HackBomm <hackbomm.net@gmail.com>
2022-08-18 13:12:37 +0600

    Form submit & reseting

--- src/Components/AddContacts.js
+++ src/Components/AddContacts.js
@@ -26,7 +26,8 @@ const AddContacts = () => {
     register,
     handleSubmit,
     setValue,
-    formState: { errors },
+    reset,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
   } = useForm({
     resolver: yupResolver(schema),
   });
@@ -36,6 +37,19 @@ const AddContacts = () => {
     setValue("dateOfBirth", startDate);
   });
 
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset({
+        firstName: "",
+        lastName: "",
+        email: "",
+        profession: "",
+        messages: "",
+        phone: "",
+        url: "",
+      });
+    }
+  });
   return (
     <div>
       <div className="container mx-auto my-20 w-1/3 bg-white">
@@ -205,6 +219,7 @@ const AddContacts = () => {
               type="submit"
               value="Send Message"
               className="focus:outline-none mt-5 bg-purple-500 px-4 py-2 text-white font-bold w-full cursor-pointer "
+              disabled={isSubmitting ? "disabled" : ""}
             />
           </form>
         </div>
